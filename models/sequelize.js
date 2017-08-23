e8df6488e08cf9ee69910df3643b7beec29e5453
Quentin <quentin@mac-pro-design.home>
2017-08-23 18:00:33 +0200

    change database conenection access

--- models/sequelize.js
+++ models/sequelize.js
@@ -2,7 +2,8 @@ var sequelize = require("sequelize");
 
 var dbName = process.env.MYSQL_DATABASE || 'ollink';
 var dbUser = process.env.MYSQL_USER || 'root';
-var dbPassword = process.env.MYSQL_PASSWORD || 'root';
+//var dbPassword = process.env.MYSQL_PASSWORD || 'root';
+var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
 var dbHost = process.env.MYSQL_SERVICE_HOST || 'localhost';
 var dbPort = process.env.MYSQL_SERVICE_PORT || 3306;
 
