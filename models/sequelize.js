ff8a319343dfe6f2d42ba5cce6cadb810cc25ac7
Quentin <quentin@mac-pro-design.home>
2017-08-24 18:28:57 +0200

    DEBUG: modifiy dbPort

--- models/sequelize.js
+++ models/sequelize.js
@@ -6,7 +6,7 @@ var dbUser = process.env.MYSQL_USER || 'root';
 var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
 //var dbHost = process.env.MYSQL_SERVICE_HOST || '127.0.0.1';
 var dbHost = '127.0.0.1';
-var dbPort = process.env.MYSQL_PORT || 3306;
+var dbPort = process.env.MYSQL_SERVICE_PORT || 3306;
 
 module.exports = new sequelize(dbName, dbUser, dbPassword, {
 	host: dbHost,
