5de1a884f3aa328ec134bde0be3ab07af0ea82d9
Quentin <quentin@mac-pro-design.home>
2017-08-24 18:39:27 +0200

    DEBUG: modifiy dbPassword

--- models/sequelize.js
+++ models/sequelize.js
@@ -2,8 +2,8 @@ var sequelize = require("sequelize");
 
 var dbName = process.env.MYSQL_DATABASE || 'ollink';
 var dbUser = process.env.MYSQL_USER || 'root';
-//var dbPassword = process.env.MYSQL_PASSWORD || 'root';
-var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
+var dbPassword = process.env.MYSQL_PASSWORD || 'root';
+//var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
 var dbHost = process.env.MYSQL_SERVICE_HOST || '127.0.0.1';
 
 var dbPort = process.env.MYSQL_SERVICE_PORT || 3306;
