fa2df22ccf2659153eb5466400d22e2e73106f09
Quentin <quentin@mac-pro-design.home>
2017-08-24 15:27:03 +0200

    add logo and rename title

--- models/sequelize.js
+++ models/sequelize.js
@@ -4,18 +4,11 @@ var dbName = process.env.MYSQL_DATABASE || 'ollink';
 var dbUser = process.env.MYSQL_USER || 'root';
 //var dbPassword = process.env.MYSQL_PASSWORD || 'root';
 var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
-var dbHost = process.env.MYSQL_SERVICE_HOST || process.env.DATABASE_URL || 'localhost';
+var dbHost = process.env.MYSQL_SERVICE_HOST || 'localhost';
 var dbPort = process.env.MYSQL_SERVICE_PORT || 3306;
 
-/*
 module.exports = new sequelize(dbName, dbUser, dbPassword, {
 	host: dbHost,
 	port: dbPort,
-	dialect: "mysql"
-});
-*/
-
-module.exports = new sequelize( dbHost, {
-	host: dbHost,
 	dialect: "mysql"
 });
