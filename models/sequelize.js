7c5b75319016e78818410ed41a5a048f7adccd0c
Quentin <quentin@mac-pro-design.home>
2017-08-24 18:17:45 +0200

    DEBUG: modifiy dbHost

--- models/sequelize.js
+++ models/sequelize.js
@@ -4,7 +4,8 @@ var dbName = process.env.MYSQL_DATABASE || 'ollink';
 var dbUser = process.env.MYSQL_USER || 'root';
 //var dbPassword = process.env.MYSQL_PASSWORD || 'root';
 var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
-var dbHost = process.env.MYSQL_SERVICE_HOST || 'localhost';
+//var dbHost = process.env.MYSQL_SERVICE_HOST || '127.0.0.1';
+var dbHost = '127.0.0.1';
 var dbPort = process.env.MYSQL_PORT || 3306;
 
 module.exports = new sequelize(dbName, dbUser, dbPassword, {
