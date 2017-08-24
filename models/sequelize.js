2ce50fa15b078e2d8f9314323768af63fd9ef3c4
Quentin <quentin@mac-pro-design.home>
2017-08-24 13:01:40 +0200

    add postgresql for heroku test

--- models/sequelize.js
+++ models/sequelize.js
@@ -4,11 +4,18 @@ var dbName = process.env.MYSQL_DATABASE || 'ollink';
 var dbUser = process.env.MYSQL_USER || 'root';
 //var dbPassword = process.env.MYSQL_PASSWORD || 'root';
 var dbPassword = process.env.MYSQL_ROOT_PASSWORD || 'root';
-var dbHost = process.env.MYSQL_SERVICE_HOST || 'localhost';
+var dbHost = process.env.MYSQL_SERVICE_HOST || process.env.DATABASE_URL || 'localhost';
 var dbPort = process.env.MYSQL_SERVICE_PORT || 3306;
 
+/*
 module.exports = new sequelize(dbName, dbUser, dbPassword, {
 	host: dbHost,
 	port: dbPort,
+	dialect: "mysql"
+});
+*/
+
+module.exports = new sequelize( dbHost {
+	host: dbHost,
 	dialect: "mysql"
 });
