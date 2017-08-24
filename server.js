cf160ac3d48d19d19014b496baacbdf3eea76abf
Quentin <quentin@mac-pro-design.home>
2017-08-24 14:50:51 +0200

    add logo

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ require("./controllers/category")(app, models);
 
 var serverPort = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
 var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-console.log(process.env);
+
 app.listen(serverPort, function() {
     console.log("Server started on port " + serverPort);
 });
